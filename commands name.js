702f36e6613f08d3c69e27bf8d6a52c7223b38dc
FOUGHTYEW <69669267+Failpfailp@users.noreply.github.com>
2020-12-17 00:36:06 +0700

    Add files via upload

---
+++ commands name.js
@@ -0,0 +1,16 @@
+//this works for all Commands like say.js, etc.
+
+//but according to the version: V12, COMMANDS HANDLERS, AND OTHERS
+
+//Discord : 𝙁𝘾 么 Glitch Editz#5631
+//Github : FOUGHTYEW/Failpfailp
+
+//#1 work Discord.js Commands handlers
+
+message.delete().catch(O_o=>{});
+return message.channel.send("Test")
+
+//#2 work Discord.js v12, Discord.js v11
+
+message.delete()
+message.channel.send("Test")
